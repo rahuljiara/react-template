6c48925b128e194c937c84a497b03f834ed47da1
Rahul Ji Ara <rahuljiaraofficial@outlook.com>
2022-12-27 14:24:34 +0530

    react blank template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar'
+import Home from "./Pages/Home"
+import Service from "./Pages/Service"
+import About from "./Pages/About"
+import Contact from "./Pages/Contact"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <BrowserRouter>
+
+    {/* 
+        link1,link2,link3,link4 is Name of Link Which Displayed In Navbar
+        connectlink1,connectlink2,connectlink3,connectlink4 is <Link to=" should be filled"/> link to pages 
+
+    */}
+    <Navbar
+        link1="Home"
+        connectlink1="/"
+
+        link2="Service"
+        connectlink2="/service"
+
+        link3="Contact"
+        connectlink3="/contact"
+
+        link4="About"
+        connectlink4="/about"
+    />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+
 }
 
 export default App;
